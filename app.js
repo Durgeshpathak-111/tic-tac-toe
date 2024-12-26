598149f29cde52c0c4fbd8c5ef4956de0e4834fc
Durgesh Pathak <durgeshpathak85814@gmail.com>
2024-12-26 22:59:08 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,159 @@
+// let boxes = document.querySelectorAll(".box");
+// let resetBtn = document.querySelector("#reset-btn");
+// let newGameBtn = document.querySelector("#new-btn");
+// let msgContainer = document.querySelector(".msgContainer");
+// let msg = document.querySelector("#msg");
+
+
+// let turnO = true; //playerX,playero
+
+// const winpatterns = [
+//     [0,1,2],
+//     [0,3,6],
+//     [0,4,8],
+//     [1,4,7],
+//     [2,5,8],
+//     [2,4,6],
+//     [3,4,5],
+//     [6,7,8],
+// ];
+// const resetGame = () => {
+//     turnO = true;
+//     enableBoxes();
+//     msgContainer.classList.add("hide");
+// };
+
+// boxes.forEach((box) => {
+//   box.addEventListener("click", () => {
+//         if (turnO) {
+//             //playerO
+//             box.innerText = "O";
+//             turnO = false;
+//         } else {
+//             //PlayerX
+//             box.innerText = "X";
+//             turnO = true;
+//         }
+//         box.disabled = true;
+
+//         checkWinner();
+//   });
+// });
+
+// const disableBoxes = () => {
+//     for (let box of boxes) {
+//         box.disabled = true;
+//     }
+// };
+
+// const enableBoxes = () => {
+//     for (let box of boxes) {
+//      box.disabled = false;
+//      box.innerText = "";
+//     }
+// };
+
+
+
+// const showWinner = (winner) => {
+//     msg.innerText = `Congratulations, Winner is ${winner}`;
+//     msgContainer.classList.remove("hide");
+//     disableBoxes();
+// };
+
+
+
+// const checkWinner = () => {
+//    for (let pattern of winpatterns) {
+//         let pos1Val = boxes[pattern[0]].innerText;
+//         let pos2Val = boxes[pattern[1]].innerText;
+//         let pos3Val = boxes[pattern[2]].innerText;
+            
+//         if (pos1Val != "" && pos2Val != "" && pos3Val != "") {
+//            if (pos1Val === pos2Val && pos2Val === pos3Val) {
+//              showWinner(pos1Val);
+//            }
+//         } 
+//      }
+// };
+
+// newGameBtn.addEventListener("click", resetGame);
+// resetBtn.addEventListener("click", resetGame);
+
+let boxes = document.querySelectorAll(".box");
+let resetBtn = document.querySelector("#reset-btn");
+let newGameBtn = document.querySelector("#new-btn");
+let msgContainer = document.querySelector(".msgContainer");
+let msg = document.querySelector("#msg");
+
+let turnO = true; // true for playerO, false for playerX
+let moveCount = 0; // to track the number of moves
+
+const winPatterns = [
+    [0, 1, 2],
+    [0, 3, 6],
+    [0, 4, 8],
+    [1, 4, 7],
+    [2, 5, 8],
+    [2, 4, 6],
+    [3, 4, 5],
+    [6, 7, 8],
+];
+
+const resetGame = () => {
+    turnO = true;
+    moveCount = 0;
+    enableBoxes();
+    msgContainer.classList.add("hide");
+};
+
+boxes.forEach((box) => {
+    box.addEventListener("click", () => {
+        if (box.innerText !== "") return; // Prevent clicking on filled boxes
+
+        box.innerText = turnO ? "O" : "X";
+        moveCount++;
+        turnO = !turnO;
+
+        checkWinner();
+    });
+});
+
+const disableBoxes = () => {
+    boxes.forEach((box) => {
+        box.disabled = true;
+    });
+};
+
+const enableBoxes = () => {
+    boxes.forEach((box) => {
+        box.disabled = false;
+        box.innerText = "";
+    });
+};
+
+const showWinner = (winner) => {
+    msg.innerText = `Congratulations, Winner is ${winner}`; // Use backticks for template literals
+    msgContainer.classList.remove("hide");
+    disableBoxes();
+};
+
+const checkWinner = () => {
+    for (let pattern of winPatterns) {
+        let pos1Val = boxes[pattern[0]].innerText;
+        let pos2Val = boxes[pattern[1]].innerText;
+        let pos3Val = boxes[pattern[2]].innerText;
+
+        if (pos1Val && pos1Val === pos2Val && pos2Val === pos3Val) {
+            showWinner(pos1Val);
+            return; // Exit the function after a winner is found
+        }
+    }
+    if (moveCount === 9) {
+        msg.innerText = "It's a draw!";
+        msgContainer.classList.remove("hide");
+    }
+};
+
+newGameBtn.addEventListener("click", resetGame);
+resetBtn.addEventListener("click", resetGame);
